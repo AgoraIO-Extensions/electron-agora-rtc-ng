54d635992f76adc59179da9cd5953f254a88c04f
zhangtao <zhangtao@agora.io>
2022-07-28 22:36:47 +0800

    feat: support observer in callApi

--- source_code/agora_node_ext/node_iris_event_handler.h
+++ source_code/agora_node_ext/node_iris_event_handler.h
@@ -2,7 +2,7 @@
  * @Author: zhangtao@agora.io
  * @Date: 2021-04-22 20:53:53
  * @Last Modified by: zhangtao@agora.io
- * @Last Modified time: 2021-10-19 14:13:45
+ * @Last Modified time: 2022-07-27 10:27:50
  */
 #pragma once
 #include <unordered_map>
@@ -14,28 +14,41 @@
 namespace agora {
 namespace rtc {
 namespace electron {
-enum CallBackModule { RTC = 0, MPK };
+enum CallBackModule { RTC = 0, MPK, OBSERVER };
+
+typedef struct NodeEventCallback {
+  napi_env env;
+  napi_ref call_back_ref;
+} EventCallback;
 
 class AgoraElectronBridge;
-class NodeIrisEventHandler : public iris::IrisEventHandler {
- public:
-  typedef struct NodeEventCallback {
-    napi_env env;
-    napi_ref call_back_ref;
-  } EventCallback;
-
+class NodeIrisEventHandler : public iris::IrisEventHandler,
+                             public iris::IrisEventHandlerEx {
  public:
   NodeIrisEventHandler();
   virtual ~NodeIrisEventHandler();
 
   // virtual void OnEvent(const char* event, const char* data) override;
 
-  
   virtual void OnEvent(const char* event,
                        const char* data,
                        const void** buffer,
                        unsigned int* length,
                        unsigned int buffer_count) override;
+
+  virtual void OnEvent(const char* event,
+                       const char* data,
+                       char result[kBasicResultLength],
+                       const void** buffer,
+                       unsigned int* length,
+                       unsigned int buffer_count) override;
+
+  void fireEvent(const char* callback_name,
+                 const char* event,
+                 const char* data,
+                 const void** buffer,
+                 unsigned int* length,
+                 unsigned int buffer_count);
 
   void addEvent(const std::string& eventName,
                 napi_env& env,
